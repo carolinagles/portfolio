d826d4594cd463125cc9f2541534d715bb5c215f
Carolina <84747399+carolinagles@users.noreply.github.com>
2021-08-07 19:34:43 -0700

    Add files via upload

---
+++ HTML-CSS-JS/content-placeholder/script.js
@@ -0,0 +1,26 @@
+const header = document.getElementById("header");
+const title = document.getElementById("title");
+const excerpt = document.getElementById("excerpt");
+const profile_img = document.getElementById("profile_img");
+const name = document.getElementById("name");
+const date = document.getElementById("date");
+
+const animated_bgs = document.querySelectorAll(".animated-bg");
+const animated_bg_texts = document.querySelectorAll(".animated-bg-text");
+
+setTimeout(getData, 2500);
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://media1.giphy.com/media/hQdWtl18ibA58nil8t/giphy.gif?cid=ecf05e47g3riog20hgvsz424l626drayobu95afciof9gygq&rid=giphy.gif&ct=g" alt="" />';
+  title.innerHTML = "Lorem ipsum dolor sit amet";
+  excerpt.innerHTML =
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis";
+  profile_img.innerHTML =
+    '<img src="https://randomuser.me/api/portraits/women/45.jpg" alt="" />';
+  name.innerHTML = "An San";
+  date.innerHTML = "Oct 08, 2020";
+
+  animated_bgs.forEach((bg) => bg.classList.remove("animated-bg"));
+  animated_bg_texts.forEach((bg) => bg.classList.remove("animated-bg-text"));
+}
